ea0e83275bd8464b3a6249279e7016868f1884ea
marzad <maschatz@gmx.net>
2022-10-26 18:34:10 +0200

    Update Main and MainTest

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,49 @@
-package org.example;public class Main {
+package org.example;
+
+import java.util.Scanner;
+
+public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        String name = getName();
+        System.out.println("\nHallo " + name);
+
+        for(int i = 0; i < 5; i++){
+            System.out.println(i + 1 + ": " + name);
+        }
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Eine Zahl: ");
+        int number = scanner.nextInt();
+
+        boolean answere = testNumber(number);
+        if(answere) {
+            System.out.println("\nJa, die Zahl " + number + " >= 0");
+        }
+        else {
+            System.out.println("\nNein, die Zahl " + number + "  < 0");
+        }
+
+        System.out.print("Die Zahl, die quadriert werden soll: ");
+
+        System.out.println("\nQuadratzahl: " + squareNumber(scanner.nextInt()));
+
     }
+    static String getName(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Wie heisst Du? ");
+        String name = scanner.next();
+        return name;
+    }
+
+    static boolean testNumber(int number){
+        if(number >= 0){
+            return true;
+        }
+        return false;
+    }
+
+    static int squareNumber(int number){
+        return number*number;
+    }
+
 }
--- src/test/java/org/example/MainTest.java
+++ src/test/java/org/example/MainTest.java
@@ -1,4 +1,26 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class MainTest {
-  
+
+    @org.junit.jupiter.api.Test
+    void testNumber() {
+        int number = 1;
+        assertTrue(Main.testNumber(number));
+    }
+
+    @org.junit.jupiter.api.Test
+    void testNumber_when_number_is_zero() {
+        int number = 0;
+        assertTrue(Main.testNumber(number));
+    }
+
+/*    @Test
+    void getName() {
+        String name = "Maria";
+        assertTrue(Main.getName() == name);
+    }*/
 }
